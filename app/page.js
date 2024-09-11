5049ffd7c1f02b1a027150ef5940ea8d42373aaa
TortoiseGhos <152567038+TortoiseGhos@users.noreply.github.com>
2024-09-11 10:05:53 -0600

    Finishing Assignment
    completing the assignment, fixing mistakes of placing folder in wrong spot and adding hover and other spice of life

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-
+    <main>
+    <h1>CPRG 306: Web Development 2 - Assignments</h1>
+    <Link className="underline text-cyan-700 hover:text-cyan-300"href="http://localhost:3000/week-2">Link to the week 2 page</Link>
+    </main>
   );
 }
--- app/week-2/new-student.js
+++ app/week-2/new-student.js
@@ -4,7 +4,7 @@ export default function NewStudent() {
     return (
         <div>
         <h1>Justin Nielsen</h1>
-        <Link href="https://github.com/TortoiseGhos/cprg306-assignments">link to github</Link>
+        <Link className="underline text-cyan-700 hover:text-cyan-300" href="https://github.com/TortoiseGhos/cprg306-assignments">link to github</Link>
         </div>
     );
   }
