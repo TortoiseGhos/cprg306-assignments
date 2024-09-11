2b2f1ad47df466e6e189c794479e1ff04bcb5f14
TortoiseGhos <152567038+TortoiseGhos@users.noreply.github.com>
2024-09-11 09:53:47 -0600

    Working on assignment
    working on 1-4 steps on assignment

---
+++ week-2/new-student.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NewStudent() {
+    return (
+        <div>
+        <h1>Justin Nielsen</h1>
+        <Link href="https://github.com/TortoiseGhos/cprg306-assignments">link to github</Link>
+        </div>
+    );
+  }
---
+++ week-2/page.js
@@ -0,0 +1,10 @@
+import NewStudent from "./new-student";
+
+export default function Page() {
+    return (
+      <main>
+        <h1>Shopping List</h1>
+        <NewStudent/>
+      </main>
+    );
+  }
